365277e14c34a0745f13298ccc179b0617a1e10e
Ian Fox <ianfox41@gmail.com>
2017-03-07 23:47:32 +0100

    Update database seed file to include login info

--- node-ds-server/scripts/seed.js
+++ node-ds-server/scripts/seed.js
@@ -2,24 +2,25 @@ const deepstream = require('deepstream.io-client-js');
 const _ = require("underscore");
 const constants = deepstream.CONSTANTS
 
-const client = deepstream(process.env.HOST).login();
+const client = deepstream(process.env.HOST).login({ username: "nodeserver", password:"1234" });
 client.on( 'connectionStateChanged', ( connectionState ) => {
   if (connectionState === constants.CONNECTION_STATE.OPEN) {
     let count;
     let completed = 0;
     let block = false;
     for ( count = 0; count < 20; count++ ) {
-      const recordName = "coins/" + String(count + 1);
+      const recordName = "coins/" + client.getUid();
       let record = client.record.getRecord(recordName);
-      console.log("CREATING" + ` coin { count }`);
+      console.log("CREATING " + recordName);
       const div = Math.pow(10, 6);
       record.whenReady(() => {
         record.set({
           location: {
             type: "Point",
-            coordinates: [ _.random(32058640, 32071833) / div, _.random(34766230, 34786015) / div ]
+            coordinates: { lng: _.random(32058640, 32071833) / div, lat: _.random(34766230, 34786015) / div }
           },
-          value: _.random(1, 5) * 10
+          value: _.random(1, 5) * 10,
+          user: ""
         },err => {
           if (err) {
             console.log('Record set with error:', err)
